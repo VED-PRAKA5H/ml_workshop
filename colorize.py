aa6d93224679b261922fa6d723e711c1cdeb3ac9
VED-PRAKA5H <vihastvideos21@gmail.com>
2025-01-01 23:56:24 +0530

    third part of workshop

---
+++ colorize.py
@@ -0,0 +1,30 @@
+import numpy as np
+import cv2
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description='Colorize Gray')
+parser.add_argument('--input', help='please give path to image')
+args = parser.parse_args()
+
+if args.input is None:
+    print('please give the imput greyscale image.')
+    print('Usage example: python fill_color.py --input greyscaleimage.png')
+    exit()
+
+if os.path.isfile(args.input) == 0:
+    print("Input file does not exist")
+    exit()
+
+frame = cv2.imread(args.input)
+protofile = "../model/"
+weightsfile = "../model/"
+pts_in_hull = np.load("../model/")
+
+net = cv2.dnn.readNetFromCaffe(protofile, weightsfile)
+
+pts_in_hull = pts_in_hull.trasnpose().reshape(2, 313, 1, 1)
+
+
+
+
